cb6c88693719a78bc9c36605d63ea23d61841425
Kevin <kphyde@gmail.com>
2016-09-17 18:41:29 -0700

    add show page and item_find before action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,11 @@
 class ItemsController < ApplicationController
-  def index
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @items = Item.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -8,7 +13,23 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
-  
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
 end
